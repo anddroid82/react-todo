9e616525b71abd940e83ab85a9e810fdec1e4596
anddroid82 <anddroid82@gmail.com>
2023-01-18 08:52:01 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Todolist from "./Todolist";
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect,useRef} from "react";
 import Bloglist from "./Bloglist";
+import { v4 as uuidv4 } from "uuid";
 
 let todoList = [
   {id:1,title:"todo1",complete:false},
@@ -8,19 +9,32 @@ let todoList = [
   {id:3,title:"todo3",complete:false}
 ];
 
+const LOCALSTORAGE_KEY = 'todoApp.todos';
+
 function App() {
+  const [posts,setPosts] = useState([]);
+  const [todos,setTodos] = useState([]);
+
+  let inputRef = useRef();
   const download = async () => {
     let x = await fetch('https://jsonplaceholder.typicode.com/posts');
     let y = await x.text();
     setPosts(JSON.parse(y));
   }
   useEffect( () => {
+    let loadedTodoList = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (loadedTodoList) {
+      setTodos(loadedTodoList);
+    }
     download();
   },[]);
 
-  const [posts,setPosts] = useState([]);
+  useEffect( () => {
+    if (todos.length === 0) return;
+    localStorage.setItem(LOCALSTORAGE_KEY,JSON.stringify(todos));
+  },[todos]);
 
-  const [todos,setTodos] = useState(todoList);
+  
   function completed(id) {
     console.log(id);
     const moddedTodos = todos.filter( todo => {
@@ -29,8 +43,18 @@ function App() {
     });
     setTodos(moddedTodos);
   }
+  function addTodo(event) {
+    event.preventDefault();
+    let inputText = inputRef.current.value;
+    if (inputText === '') return;
+    let newTodos = [...todos,{id:uuidv4(),title:inputText,complete:false}];
+    setTodos(newTodos);
+    inputRef.current.value = '';
+  }
+
   return (
     <div className="App">
+      <input type="text" ref={inputRef} /><button onClick={addTodo}>Hozzáad</button>
       <Todolist todos={todos} complete={completed} />
       <Bloglist posts={posts} />
     </div>
